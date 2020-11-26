16edfb76991c45ee79ead15f6f371db5f50e2cf3
CoolNamesAllTaken <jmcnelly@stanford.edu>
2020-11-25 21:23:38 -0800

    Build basic framework
    * Transforms in Octave.
    * Nucleo firmware:
    	* Implement test framework.
    	* Implement Encoder and AS5048A classes.
    	* Implement basic program structure with FreeRTOS.
    	* Bind utility functions like printf, configure project settings.

---
+++ firmware/.metadata/.plugins/org.eclipse.core.resources/.history/6d/5079390f232e001b16ebbedc617f97c8
@@ -0,0 +1,4 @@
+App/Encoder/Src/encoder.o: ../App/Encoder/Src/encoder.cc \
+ ../App/Encoder/Inc/encoder.hh
+
+../App/Encoder/Inc/encoder.hh:
---
+++ firmware/nucleo/Debug/App/Tests/Src/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../App/Tests/Src/test_encoder.cc 
+
+CC_DEPS += \
+./App/Tests/Src/test_encoder.d 
+
+OBJS += \
+./App/Tests/Src/test_encoder.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+App/Tests/Src/test_encoder.o: ../App/Tests/Src/test_encoder.cc
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DUSE_HAL_DRIVER -DSTM32F722xx -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc -I../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1 -I../App -I../App/Tests/Inc -I../App/Encoder/Inc -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"App/Tests/Src/test_encoder.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
